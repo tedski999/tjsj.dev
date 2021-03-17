ae0ee1dd260f0cb81f58ed1e9c5ad2d0e967d404
tedski999 <tedjohnsonjs@gmail.com>
2021-03-17 02:28:25 +0000

    Added projects and posts routes
    Added projects and posts routes to router.go
    Added basic HTML templates for each route
    Added a bunch of TODOs

--- src/router.go
+++ src/router.go
@@ -1,10 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/projects/", projectsHandler)
+	mux.HandleFunc("/posts/", postsHandler)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	return mux
 }
@@ -15,7 +20,32 @@ func homeHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// TODO: pass list of recent projects and posts into the template
 	ExecuteTemplate(w, "home.html", nil)
+}
+
+func projectsHandler(w http.ResponseWriter, req *http.Request) {
+	selection := strings.TrimPrefix(req.URL.Path, "/projects/")
+	if len(selection) == 0 {
+		// TODO: pass the list of projects into the template
+		ExecuteTemplate(w, "projects.html", nil)
+	} else {
+		// TODO: find the project with a name equal to selection
+		// TODO: if none found, 404
+		errorHandler(w, req, http.StatusNotFound)
+	}
+}
+
+func postsHandler(w http.ResponseWriter, req *http.Request) {
+	selection := strings.TrimPrefix(req.URL.Path, "/posts/")
+	if len(selection) == 0 {
+		// TODO: pass the list of posts into the template
+		ExecuteTemplate(w, "posts.html", nil)
+	} else {
+		// TODO: find the post with a name equal to selection
+		// TODO: if none found, 404
+		errorHandler(w, req, http.StatusNotFound)
+	}
 }
 
 func errorHandler(w http.ResponseWriter, req *http.Request, code int) {

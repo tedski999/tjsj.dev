a3fba49e0f5e2a7f6173f7ab999489719e0a4b36
tedski999 <tedjohnsonjs@gmail.com>
2021-03-22 12:36:18 +0000

    Better title goofs error handling

--- src/content/titlegoofs.go
+++ src/content/titlegoofs.go
@@ -10,22 +10,23 @@ import (
 var titleGoofs []string
 
 // Load each line as a separate goof
-func LoadTitleGoofs(filepath string) error {
+func LoadTitleGoofs(filepath string) {
 	rand.Seed(time.Now().Unix())
 
 	file, err := os.Open(filepath)
 	if err != nil {
-		return err
+		panic(err.Error())
 	}
+	defer file.Close()
 
 	titleGoofs = nil
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		titleGoofs = append(titleGoofs, scanner.Text())
 	}
-
-	file.Close()
-	return scanner.Err()
+	if len(titleGoofs) == 0   {
+		panic("Could not load any title goofs from " + filepath)
+	}
 }
 
 // Return a randomly picked goof from the loaded title goofs

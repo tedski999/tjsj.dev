1ca162487267cb48690e75748af1dcab0533fdb5
tedski999 <tedjohnsonjs@gmail.com>
2021-04-18 21:58:20 +0100

    Move goroutine to method
    Moved the web request handler goroutine in
    webserver/control.go to its own method.

--- pkg/webserver/control.go
+++ pkg/webserver/control.go
@@ -8,15 +8,8 @@ import (
 // Start the server on a separate goroutine
 func (server *Server) Start(errChan chan<- error) {
 	server.errChan = errChan
-
 	server.doneWG.Add(1)
-	go func () {
-		defer server.doneWG.Done()
-		err := server.http.ListenAndServeTLS(server.certFilePath, server.keyFilePath)
-		if err != http.ErrServerClosed {
-			server.errChan <- err
-		}
-	}()
+	go server.webRequestHander()
 }
 
 // Gracefully shutdown the server
@@ -24,4 +17,12 @@ func (server *Server) Stop() error {
 	err := server.http.Shutdown(context.Background())
 	server.doneWG.Wait()
 	return err
+}
+
+func (server *Server) webRequestHander() {
+	defer server.doneWG.Done()
+	err := server.http.ListenAndServeTLS(server.certFilePath, server.keyFilePath)
+	if err != http.ErrServerClosed {
+		server.errChan <- err
+	}
 }

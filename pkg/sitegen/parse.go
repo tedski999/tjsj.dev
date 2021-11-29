09366664e19eb5e7049c09c7eab79922bc4d52c0
tedski999 <tedjohnsonjs@gmail.com>
2021-11-29 03:15:06 +0000

    Cleanup sitegen parse

--- pkg/sitegen/parse.go
+++ pkg/sitegen/parse.go
@@ -1,40 +1,30 @@
 package sitegen
 
 import (
-	"os"
 	"io/ioutil"
 	"encoding/json"
 )
 
-func ParseSiteTemplateFile(templateFile string) (SiteTemplateFormat, error) {
+func ParseSiteTemplateFile(file string) (SiteTemplateFormat, error) {
 	var siteTemplate SiteTemplateFormat
-	file, err := os.Open(templateFile)
-	if err != nil { return siteTemplate, err }
-	bytes, err := ioutil.ReadAll(file)
-	file.Close()
+	bytes, err := ioutil.ReadFile(file)
 	if err != nil { return siteTemplate, err }
 	err = json.Unmarshal(bytes, &siteTemplate)
 	if err != nil { return siteTemplate, err }
 	return siteTemplate, nil
 }
 
-func ParseSiteFile(siteFile string) (SiteFormat, error) {
+func ParseSiteFile(file string) (SiteFormat, error) {
 	var site SiteFormat
-	file, err := os.Open(siteFile)
-	if err != nil { return site, err }
-	bytes, err := ioutil.ReadAll(file)
-	file.Close()
+	bytes, err := ioutil.ReadFile(file)
 	if err != nil { return site, err }
 	err = json.Unmarshal(bytes, &site)
 	if err != nil { return site, err }
 	return site, nil
 }
 
-func ParseDataFile(dataFile string) (map[string]interface{}, error) {
-	file, err := os.Open(dataFile)
-	if err != nil { return nil, err }
-	bytes, err := ioutil.ReadAll(file)
-	file.Close()
+func ParseDataFile(file string) (map[string]interface{}, error) {
+	bytes, err := ioutil.ReadFile(file)
 	if err != nil { return nil, err }
 	var data map[string]interface{}
 	err = json.Unmarshal(bytes, &data)
